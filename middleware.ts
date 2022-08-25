5f79b3ceff3e5f195e389d6847f014723b70cdba
Zachary Salmon <zak@usefirstmate.xyz>
2022-08-25 17:34:10 -0400

    update middleware file with domains

--- middleware.ts
+++ middleware.ts
@@ -45,7 +45,7 @@ export default function middleware(req: NextRequest) {
   const currentHost =
     process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
       ? hostname
-          .replace(`.vercel.pub`, "")
+          .replace(`.platform-test-nine.vercel.app`, "")
           .replace(`.platformize.vercel.app`, "")
       : hostname.replace(`.localhost:3000`, "");
   // rewrites for app pages
